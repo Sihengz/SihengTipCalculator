f39a5f9ef6636713f7e7edf2ba915a636d2a9f77
BT_1N3_30 <sample@bths.com>
2023-09-21 08:39:23 -0400

    Inputs

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,21 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
         System.out.println("Welcome to the tip calculator");
-        System.out.print("What's the tip percentage");
+        System.out.print("How many people are in your group:");
+        int people = scan.nextInt();
+        scan.nextLine();
+        System.out.print("What's the tip percentage (1-100)");
+        int tipPercent = scan.nextInt();
+        scan.nextLine();
 
+        double totalCost = 0;
+        double currentItemCost = 0;
+        while (currentItemCost != -1.0) {
+            System.out.print("Enter a cost in  dollars and cents, e.g. 12.50 (-1 to end)");
+            currentItemCost = scan.nextDouble();
+
+        }
     }
 }
