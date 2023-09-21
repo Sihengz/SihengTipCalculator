f7a2a3da144f0ef707013eba981c7b2dbb58a061
BT_1N3_30 <sample@bths.com>
2023-09-21 08:31:33 -0400

    Init

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the tip calculator");
+        System.out.print("What's the tip percentage");
+
+    }
+}
