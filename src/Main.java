c33c465aab323bd0a60014d3f09240bb41b87445
BT_1N3_30 <sample@bths.com>
2023-09-21 09:02:39 -0400

    rounding pt.1

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,25 @@
 import java.util.Scanner;
+import java.text.DecimalFormat;
 
 public class Main {
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
+        String pattern = "###,###.##";
+        DecimalFormat decimalFormat = new DecimalFormat(pattern);
+
         System.out.println("Welcome to the tip calculator");
-        System.out.print("How many people are in your group:");
+        System.out.print("How many people are in your group: ");
         int people = scan.nextInt();
         scan.nextLine();
-        System.out.print("What's the tip percentage (1-100)");
+        System.out.print("What's the tip percentage (1-100): ");
         int tipPercent = scan.nextInt();
         scan.nextLine();
 
         double totalCost = 0;
         double currentItemCost = 0;
         while (currentItemCost != -1.0) {
-            System.out.print("Enter a cost in  dollars and cents, e.g. 12.50 (-1 to end)");
+            System.out.print("Enter a cost in  dollars and cents, e.g. 12.50 (-1 to end): ");
             currentItemCost = scan.nextDouble();
             if (currentItemCost != -1.0) {
                 totalCost += currentItemCost;
@@ -24,7 +29,7 @@ public class Main {
         System.out.println("Total bill before tip: $" + totalCost);
         System.out.println("Total percentage: " + tipPercent + "%");
         double tip = (tipPercent / 100.0) * totalCost;
-        System.out.println("Total tip: $" + (tip));
+        System.out.println("Total tip: $" + (decimalFormat.format(tip)));
         double finalBill = totalCost + tip;
         System.out.println("Total bill with tip: $" + finalBill);
         double perPersonCost = totalCost / people;
